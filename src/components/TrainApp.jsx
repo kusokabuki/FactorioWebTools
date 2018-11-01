fb2126cacd147c96425f7f60de4211ce4633b9a8
Meowhal <gnsk.sz@gmail.com>
2018-11-02 03:10:30 +0900

    制動距離計算を追加

--- src/components/TrainApp.jsx
+++ src/components/TrainApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { debug } from 'util';
 
 export default class TrainApp extends React.Component {
     constructor(props) {
@@ -10,20 +11,25 @@ export default class TrainApp extends React.Component {
             { name: "核燃料", acc_mult: 2.5, top_speed_mult: 1.15 },
         ];
 
+        this.breakings =[
+            0, 0.1, 0.25, 0.40, 0.55, 0.70, 0.85, 1.00 
+        ];
+
         this.vehicles = [
-            { id:"locomotive_active", name: "機関車（前向き）", min:1, weight: 2000, max_speed: 1.2, max_power: 10, friction_force: 0.5, air_resistance: 0.0075},
-            { id:"locomotive_inactive", name: "機関車（後向き）", min:0, weight: 2000, max_speed: 1.2, max_power: 0, friction_force: 0.5, air_resistance: 0.0075},
-            { id:"cargo_wagon", name: "貨物車両", min:0, weight: 1000, max_speed: 1.5, max_power: 0, friction_force: 0.5, air_resistance: 0.01},
-            { id:"artillery_wagon", name: "列車砲", min:0, weight: 4000, max_speed: 1.5, max_power: 0, friction_force: 0.5, air_resistance: 0.015},
+            { id:"locomotive_active", name: "機関車（前向き）", min:1, weight: 2000, max_speed: 1.2, max_power: 10, friction_force: 0.5, air_resistance: 0.0075, braking_force:10},
+            { id:"locomotive_inactive", name: "機関車（後向き）", min:0, weight: 2000, max_speed: 1.2, max_power: 0, friction_force: 0.5, air_resistance: 0.0075, braking_force:10},
+            { id:"cargo_wagon", name: "貨物車両", min:0, weight: 1000, max_speed: 1.5, max_power: 0, friction_force: 0.5, air_resistance: 0.01, braking_force:3},
+            { id:"artillery_wagon", name: "列車砲", min:0, weight: 4000, max_speed: 1.5, max_power: 0, friction_force: 0.5, air_resistance: 0.015, braking_force:3},
         ];
 
         this.state = {
             fuel: 0,
+            breaking_bonus: 0,
             locomotive_active: 1,
             locomotive_inactive: 0,
             cargo_wagon: 0,
             artillery_wagon: 0,
-            Leadertype: 0 //0=locomotive, 1=cargo        
+            Leadertype: 0 //0=locomotive, 1=cargo
         }
 
         this.handleChangeInput = this.handleChangeInput.bind(this);
@@ -49,6 +55,17 @@ export default class TrainApp extends React.Component {
                     >
                         {this.fuels.map((f, i) => 
                             <option key={f.name} value={i}>{f.name}</option>
+                        )}
+                    </select>
+                </p>
+                <p>
+                    <label>制動力ボーナス：</label>
+                    <select
+                        value={this.state.breaking_bonus}
+                        onChange={e => this.setState({ breaking_bonus: Number.parseFloat(e.target.value)})}
+                    >
+                        {this.breakings.map((f, i) => 
+                            <option key={f} value={f}>{Math.round(f * 100)} %</option>
                         )}
                     </select>
                 </p>
@@ -78,10 +95,6 @@ export default class TrainApp extends React.Component {
                 </p>
                 <hr></hr>
                 <CalcResult result={ this.calculate() }></CalcResult>
-                <span>
-                    ※注意：燃料の補充時に加速が鈍るため結果が一致しないことがあります。
-                </span>
-
             </div>            
         );
     }
@@ -90,12 +103,14 @@ export default class TrainApp extends React.Component {
         let weight = 0; // 重量
         let friction = 0; // 摩擦
         let power = 0; // 動力
+        let breaking = 0; // 制動力
 
         this.vehicles.forEach(v => {
             let n = this.state[v.id];
             weight += v.weight * n;
             friction += v.friction_force * n;
             power += v.max_power * n;
+            breaking += v.braking_force * (this.state.breaking_bonus + 1) * n;
         });
         
         // 空気抵抗
@@ -110,41 +125,50 @@ export default class TrainApp extends React.Component {
             return NaN;
         }
 
-        // 出発時から n tick目の速度を v(n)、
-        // n tick目で空気抵抗なしで加速した場合の速度を v'(n)とすると
-        //   v'(n) = v(n) + (動力 - 摩擦) / 重量
-        //   v(n + 1) = v'(n) - v'(n) * 空気抵抗 / 重量
-        // v'(n)を取り除くと
-        //   v(n+1) = (v(n) + (動力 - 摩擦) / 重量) / (1 - 空気抵抗 / 重量)
-        // (1 - 空気抵抗 / 重量)を p, (動力 - 摩擦) / 重量) / p を q とすると
+        // Factorioの列車は動力による加速は現在の速度によらず一定で、空気抵抗は速度に比例して大きくなるようである。
+        // 出発時から n tick目の速度を v(n)、加速を accとすると
+        //   acc = (動力 - 摩擦) / 重量
+        //   v(n + 1) = (v(n) + acc) * (1 - 空気抵抗 / 重量)
+        // となることが観察により確認された。
+        // (1 - 空気抵抗 / 重量)を p, acc / p を q とすると
         //   v(n+1) = p * v(n) + q
-        // 漸化式の一般項より
+        // で、この漸化式の一般項は
         //   v(n) = (v(1) - a) * p ^ (n - 1) + a
         //             (a = q / (1 - p))
-        // 最高速度 max に達するのは v(n_max) > max のときなのでこれを n_max について解くと
-        //   (v1 - a) * p ^ (n_max - 1) + a > max
-        //   n_max > log_p((max - a) * p / (v1 - a)
+        // となる。ここで a は v(n+1) = v(n) = a 。つまり a は最高速度である。
+        // ただし、ゲーム内では速度の制限が設定されているので、最高速度 s_max は a と 制限速度のうち小さい方とする。
+        // 最高速度 s_max に達するのは v(n_max) > s_max のときなのでこれを n_max について整理すると
+        //   (v1 - a) * p ^ (n_max - 1) + a > s_max
+        //   n_max > log_p((s_max - a) * p / (v1 - a)
         // 現在位置は各tickごと速度v(n) だけ移動するので n tick 後の位置は
         //   ∑ v(k) {k = 1,n}
         //   = (v1 - a) * (p ^ n - 1) / (p - 1) + n * a
-        // 以上により最高速度到達時の位置を計算できる
+        // となる。ここにn_maxを代入すれば最高速度到達時の位置が求まる。
 
         const p = (1 - airResistance / weight * 1000);
         const q = (power * acc_mult - friction) / weight * p;
         const v1 = power * acc_mult / weight * p;
         const a = q / (1 - p); // 特性方程式(a は v(n+1) = v(n) のときの解)
 
-        let maxSpeed = Math.min(a-0.0000001, limitSpeed);
+        // 特性方程式の解(a)は到達可能な最高速度　a をそのまま使うとｎが求まらないので少し小さい値にする
+        const maxSpeed = Math.min(a-0.0000001, limitSpeed);
 
         const maxn = Math.log((maxSpeed - a) * p / (v1 - a)) / Math.log(p);
         const distance = (v1 - a) * (Math.pow(p, maxn) - 1) / (p - 1) + maxn * a;
+
+        // 制動距離計算
+        // https://forums.factorio.com/viewtopic.php?t=60134 より
+        // Braking distance (in tiles): max( ½ * weight * speed / (braking + friction), 1.1) * speed + 2
+
+        const bd = Math.max(weight * maxSpeed / (breaking + friction) / 2, 1.1) * maxSpeed + 2;
 
         return {
             limitSpeed: limitSpeed,
             maxSpeed: maxSpeed,
             tick_reached_max: maxn,
             distance: distance,
-            weight: weight
+            weight: weight,
+            breaking_discance: bd
         }
     }
 }
@@ -154,7 +178,7 @@ const CalcResult = (props) => {
     if (Number.isNaN(props.result)) {
         return <div>この構成では列車は動きません.</div>
     } else {
-        const { limitSpeed, maxSpeed, tick_reached_max, distance, weight} = props.result;
+        const { limitSpeed, maxSpeed, tick_reached_max, distance, weight, breaking_discance} = props.result;
         return (
             <div>
                 <table>
@@ -176,8 +200,12 @@ const CalcResult = (props) => {
                             <td>{ Math.ceil(distance / 2)} 個</td>
                         </tr>
                         <tr>
-                            <th>時間</th>
+                            <th>最高速度到達時間</th>
                             <td>{tick_reached_max / 60 } 秒</td>
+                        </tr>
+                        <tr>
+                            <th>最高速度時の制動距離</th>
+                            <td>{breaking_discance} m</td>
                         </tr>
                         <tr>
                             <th>総重量</th>
