572445e54f793bdef963daeed77b87486c911df6
Meowhal <gnsk.sz@gmail.com>
2018-11-18 00:02:40 +0900

    ajaxテスト用にノードサーバーを構築

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {    
+    const urlParts = url.parse(req.url);
+    const p = path.join(__dirname, "../docs/", urlParts.pathname);
+    
+    const stream = fs.createReadStream(p);
+    stream.on('error', (err) => {
+        console.log(err.message);
+    });
+    stream.on("end", (chunk) =>{
+        console.log("sent " + p);
+    })
+    stream.pipe(res);
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
